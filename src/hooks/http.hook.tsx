62feadfb80dbb967863547bc9842cd6a6d09110d
Oleg <oleg.ustinov.1993@mail.ru>
2023-10-11 13:49:33 +0600

    http request

---
+++ src/hooks/http.hook.tsx
@@ -0,0 +1,51 @@
+import { useState, useCallback } from 'react';
+
+type HttpRequestMethods = 'GET' | 'POST' | 'PATCH' | 'DELETE';
+
+interface HttpHeader {
+    [key: string]: string
+}
+
+// type HttpHeader = Record<string, string>;
+
+interface RequestConfig {
+    url: string,
+    method: HttpRequestMethods,
+    body?: string | null,
+    headers: HttpHeader
+}
+
+export const useHttp = () => {
+    const [loadingStatus, setLoadingStatus] = useState<string>('idle');
+
+
+    const request = useCallback(
+        async ({ url,
+            method = "GET",
+            body = null,
+            headers = { "Content-type": "application/json" } }: RequestConfig
+        ) => {
+            setLoadingStatus('loading');
+
+            try {
+                const response = await fetch(url, { method, body, headers });
+
+                if (!response.ok) {
+                    throw new Error(`Could not fetch ${url}, status: ${response.status}`);
+                }
+
+
+                const data = await response.json();
+
+                setLoadingStatus('idle');
+
+                return data;
+            } catch (e) {
+                setLoadingStatus('error');
+                throw e;
+            }
+
+        }, []);
+    return { request, loadingStatus };
+}
+
---
+++ src/services/AppointmentService.ts
@@ -0,0 +1,10 @@
+import { useHttp } from "../hooks/http.hook";
+
+
+const AppointmentService = () => {
+    const { loadingStatus, request } = useHttp();
+
+    const getAllAppointments = async () => {
+
+    }
+}
